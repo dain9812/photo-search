0f03c9b99bc47895d4a5b0e63f8b377396a6e079
hamdain <dain9812@naver.com>
2022-05-19 00:42:16 +0900

    feat: Get curated photos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Header from "../components/Header";
+import Photos from "../components/Photos";
 
 const Container = styled.div``;
 
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <Container>
       <Header />
+      <Photos />
     </Container>
   );
 };
---
+++ src/utils/Api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_KEY = "563492ad6f917000010000016002d567f8934047bff69b75cadb67bb";
+const BASE_URL = "https://api.pexels.com/v1/";
+
+// GET https://api.pexels.com/v1/curated?page=2&per_page=40
+export const getCuratedPhotos = async (page, perPage) => {
+  const response = await axios.get(
+    `${BASE_URL}curated?page=${page}&per_page=${perPage}`,
+    {
+      headers: {
+        Authorization: API_KEY,
+      },
+    }
+  );
+  const data = await response.data;
+  return data.photos;
+};
