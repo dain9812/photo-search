7a369a89f82a4ad0f6649156c96d03add4439d02
hamdain <dain9812@naver.com>
2022-05-24 16:23:17 +0900

    feat: Add photo hover effect

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 
 const Li = styled.li`
   width: 31%;
-  padding: 20px 0;
+  margin: 20px 0;
   box-sizing: border-box;
+  position: relative;
+  border-radius: 32px;
+  overflow: hidden;
   @media screen and (max-width: 500px) {
     width: 48%;
   }
@@ -14,11 +17,23 @@ const Img = styled.img`
   width: 100%;
 `;
 
+const Shadow = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  &:hover {
+    background: rgba(0, 0, 0, 0.5);
+  }
+`;
+
 const ListItem = ({ id, src, alt }) => {
   return (
     <Li>
       <Link to={`photo/${id}`}>
         <Img src={src.portrait} alt={alt} />
+        <Shadow />
       </Link>
     </Li>
   );
